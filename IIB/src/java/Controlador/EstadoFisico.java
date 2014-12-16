8a34fe19f0ce161239dd2f2fdf9b53ebc5ee515f
Elizabeth <elizheralf@gmail.com>
2014-12-15 21:30:35 -0600

    Comentario de Estado Físico

--- IIB/src/java/Controlador/EstadoFisico.java
+++ IIB/src/java/Controlador/EstadoFisico.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Clase para el manejo del catalogo Estado Físico.
  */
 
 package Controlador;
@@ -12,29 +10,46 @@ package Controlador;
  */
 public class EstadoFisico {
     
+    /*Id del estado físico*/
     private int idEstadoFisico;
+    /*Descripción del estado físico*/
     private String descripcion;
 
     public EstadoFisico() {
     }
 
+/*
+ *Constructor que recibe un entero que representa el id del elemento y una cadena que representa el nombre del elemeto.
+ */
     public EstadoFisico(int idEstadoFisico, String descripcion) {
         this.idEstadoFisico = idEstadoFisico;
         this.descripcion = descripcion;
     }
 
+/*
+ *Método get que regresa el id del Estado Físico.. 
+ */
     public int getIdEstadoFisico() {
         return idEstadoFisico;
     }
 
+/*
+ *Asigna el id del estado Físico.
+ */
     public void setIdEstadoFisico(int idEstadoFisico) {
         this.idEstadoFisico = idEstadoFisico;
     }
 
+/*
+ *Método get que regresa el nombre del Estado Físico.
+ */
     public String getDescripcion() {
         return descripcion;
     }
 
+/*
+ *Asigna el nombre del Estado Físico.
+ */
     public void setDescripcion(String descripcion) {
         this.descripcion = descripcion;
     }
