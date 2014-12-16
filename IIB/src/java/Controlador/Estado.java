478746ebcc117feeaff2de2b839e7a239dbe977e
Elizabeth <elizheralf@gmail.com>
2014-12-15 21:33:38 -0600

    Comentario de Estado

--- IIB/src/java/Controlador/Estado.java
+++ IIB/src/java/Controlador/Estado.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Clase para el manejo del catalogo Estado.
  */
 
 package Controlador;
@@ -12,29 +10,46 @@ package Controlador;
  */
 public class Estado {
     
+    /*Id del estado*/
     private int idEstado;
+    /*Descripción del estado*/
     private String descripcion;
 
     public Estado() {
     }
 
+/*
+ *Constructor que recibe un entero que representa el id del elemento y una cadena que representa el nombre del elemeto.
+ */
     public Estado(int idEstado, String descripcion) {
         this.idEstado = idEstado;
         this.descripcion = descripcion;
     }
 
+/*
+ *Método get que regresa el id del Estado. 
+ */
     public int getIdEstado() {
         return idEstado;
     }
 
+/*
+ *Asigna el id del Estado..
+ */
     public void setIdEstado(int idEstado) {
         this.idEstado = idEstado;
     }
 
+/*
+ *Método get que regresa el nombre del Estado.
+ */
     public String getDescripcion() {
         return descripcion;
     }
 
+/*
+ *Asigna el nombre del Estado.
+ */
     public void setDescripcion(String descripcion) {
         this.descripcion = descripcion;
     }
